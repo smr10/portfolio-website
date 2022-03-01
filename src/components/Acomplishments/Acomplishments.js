888ff7e4895ca2d65639e5cd4e2661e0d09b081b
smr10 <shamirnun@gmail.com>
2022-03-01 13:13:24 +0400

    PA

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -9,18 +9,19 @@ import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 import { Acomplishment } from "../../constants/constants";
 
 const Acomplishments = () => (
-  <Section>
-    <SectionTitle>Personal Acomplishments</SectionTitle>
-    <Boxes>
-      {Acomplishment.map((card, index) => (
-        <Box key={index}>
-          <BoxNum>
-            {card.number}+<BoxText>{card.text}</BoxText>
-          </BoxNum>
-        </Box>
-      ))}
-    </Boxes>
-  </Section>
+  // <Section>
+  //   <SectionTitle>Personal Acomplishments</SectionTitle>
+  //   <Boxes>
+  //     {Acomplishment.map((card, index) => (
+  //       <Box key={index}>
+  //         <BoxNum>
+  //           {card.number}+<BoxText>{card.text}</BoxText>
+  //         </BoxNum>
+  //       </Box>
+  //     ))}
+  //   </Boxes>
+  // </Section>
+  <div />
 );
 
 export default Acomplishments;
